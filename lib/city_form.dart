3eba1e16842aa127864ea8a37a123604be98b7a5
JulienJes <91883647+JulienJes@users.noreply.github.com>
2024-04-12 17:15:41 +0200

    bycoord ok

--- lib/city_form.dart
+++ lib/city_form.dart
@@ -12,17 +12,29 @@ class CityForm extends StatefulWidget {
 }
 
 class _CityFormState extends State<CityForm> {
-  final TextEditingController _controller = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerLatitude = TextEditingController();
+  final TextEditingController _controllerLongitude = TextEditingController();
+  final _formKeyName = GlobalKey<FormState>();
+  final _formKeyCoordinates = GlobalKey<FormState>();
   Future<Map<String, dynamic>>? _future;
 
-  Future<Map<String, dynamic>> _getCityWeather(String city) async {
+  Future<Map<String, dynamic>> _getCityWeatherByName(String city) async {
     var responseCity = await fetchCity(city);
     var responseWeather =
         await fetchWeather(responseCity.latitude, responseCity.longitude);
 
     return {
       'cityData': responseCity,
+      'weatherData': responseWeather,
+    };
+  }
+
+  Future<Map<String, dynamic>> _getCityWeatherByCoordinates(
+      double latitude, double longitude) async {
+    var responseWeather = await fetchWeather(latitude, longitude);
+
+    return {
       'weatherData': responseWeather,
     };
   }
@@ -37,11 +49,11 @@ class _CityFormState extends State<CityForm> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Form(
-            key: _formKey,
+            key: _formKeyName,
             child: Column(
               children: <Widget>[
                 TextFormField(
-                  controller: _controller,
+                  controller: _controllerName,
                   decoration: const InputDecoration(labelText: 'City'),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -56,15 +68,75 @@ class _CityFormState extends State<CityForm> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    if (_formKey.currentState != null &&
-                        _formKey.currentState!.validate()) {
+                    if (_formKeyName.currentState != null &&
+                        _formKeyName.currentState!.validate()) {
                       setState(() {
-                        _future = _getCityWeather(_controller.text);
+                        _future = _getCityWeatherByName(_controllerName.text);
                       });
                     } else {
                       ScaffoldMessenger.of(context).showSnackBar(
                         const SnackBar(
                           content: Text('Veuillez entrer une ville valide.'),
+                          duration: Duration(seconds: 4),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+          Form(
+            key: _formKeyCoordinates,
+            child: Column(
+              children: <Widget>[
+                Column(
+                  children: [
+                    TextFormField(
+                      controller: _controllerLatitude,
+                      decoration: const InputDecoration(labelText: 'Latitude'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer une coordonnée';
+                        }
+                        final regex = RegExp(r'^[0-9]+(\.[0-9]+)?$');
+                        if (!regex.hasMatch(value)) {
+                          return 'Veuillez entrer une coordonnée valide';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: _controllerLongitude,
+                      decoration: const InputDecoration(labelText: 'Longitude'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Veuillez entrer une coordonnée';
+                        }
+                        final regex = RegExp(r'^[0-9]+(\.[0-9]+)?$');
+                        if (!regex.hasMatch(value)) {
+                          return 'Veuillez entrer une coordonnée valide';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKeyCoordinates.currentState != null &&
+                        _formKeyCoordinates.currentState!.validate()) {
+                      setState(() {
+                        _future = _getCityWeatherByCoordinates(
+                            double.parse(_controllerLatitude.text),
+                            double.parse(_controllerLongitude.text));
+                      });
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content:
+                              Text('Veuillez entrer des coordonnées valides.'),
                           duration: Duration(seconds: 4),
                         ),
                       );
@@ -96,16 +168,22 @@ class _CityFormState extends State<CityForm> {
                   var weatherData = snapshot.data!['weatherData'];
                   return Column(
                     children: [
-                      Text(cityData.name),
-                      Text(cityData.country),
+                      cityData != null
+                          ? Text(cityData.name ?? "Nom indisponible")
+                          : Container(),
+                      cityData != null
+                          ? Text(cityData.country ?? "Pays indisponible")
+                          : Container(),
                       Text(weatherData.main),
                       Text(weatherData.temp.toString()),
                       const SizedBox(height: 20),
                       Expanded(
                         child: FlutterMap(
                           options: MapOptions(
-                            initialCenter:
-                                LatLng(cityData.latitude, cityData.longitude),
+                            initialCenter: cityData != null
+                                ? LatLng(cityData.latitude, cityData.longitude)
+                                : LatLng(double.parse(_controllerLatitude.text),
+                                    double.parse(_controllerLongitude.text)),
                             initialZoom: 13.0,
                           ),
                           children: [
