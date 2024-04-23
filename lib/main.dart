8626fb2b9880ae0c97f97e366217c8ed9e82961a
JulienJes <91883647+JulienJes@users.noreply.github.com>
2024-04-23 21:53:01 +0200

    splash & icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:meteo/weather.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load(fileName: ".env");
   runApp(const Weather());
 }
--- lib/weather.dart
+++ lib/weather.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meteo/city_form.dart';
 
-class Weather extends StatelessWidget {
+class Weather extends StatefulWidget {
   const Weather({super.key});
 
   @override
+  State<Weather> createState() => _Weather();
+}
+
+class _Weather extends State<Weather> {
+  // bool _isInitialized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+    // setState(() {
+    //   _isInitialized = true;
+    // });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Weather', home: CityForm());
+    return const MaterialApp(title: 'Weather', home: CityForm()
+        // : Container(
+        //     color: Colors.white,
+        //     child: const Center(
+        //       child: SplashScreen(),
+        //     ),
+        //   ),
+        );
   }
 }
